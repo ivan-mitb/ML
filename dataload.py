520071c376399eff6344e78d0907edca4629af14
ivan <40048539+ivan-mitb@users.noreply.github.com>
2018-07-11 00:26:36 +0800

    forgot to stack cols 37: of x_train/x_test

--- dataload.py
+++ dataload.py
@@ -142,7 +142,7 @@ def make_reduce(x_train, x_test, y_train, y_test):
     pca = PCA(n_components = 5, random_state = 4129)
     pca_result = pca.fit_transform(x_train)
     # join the 5 princomps with the 11 best columns
-    x_train = np.hstack((pca_result, x_train[:, col_list]))
-    x_test = np.hstack((pca.transform(x_test), x_test[:, col_list]))
+    x_train = np.hstack((pca_result, x_train[:, col_list], x_train[:, 37:]))
+    x_test = np.hstack((pca.transform(x_test), x_test[:, col_list], x_test[:, 37:]))
     # make REDUCE.DAT
     save_object([x_train, x_test, y_train, y_test], 'reduce.dat')
